22886a1cbbcd1722710d79c3ecf4f535862ef817
Aurelia <aurelia-bot@aurelia.com>
2026-01-23 21:49:52 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -81,7 +81,9 @@ function runSetupCommands() {
     "bun add react-intersection-observer@^9.8.0",
     "bun add react-masonry-css@^1.0.16",
     "bun add react-dropzone@^14.2.3",
-    "bun add date-fns"
+    "bun add date-fns",
+    "bun add @radix-ui/react-dialog",
+    "bun add @radix-ui/react-slot"
 ];
     
     if (commands.length === 0) {
